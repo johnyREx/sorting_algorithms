bf21e8f734447da2c430709394ba5f8b4b12960d
johnyREx <johnkwofie99@icloud.com>
2023-09-23 10:30:00 -0700

    betty errors fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,7 +3,7 @@
 /**
  * bubble_sort - Sorts an array of integers in order by bubble sort
  * @array: The array to be sorted
- * @size: Number of elements in the array 
+ * @size: Number of elements in the array
  */
 
 void bubble_sort(int *array, size_t size)
@@ -18,11 +18,12 @@ void bubble_sort(int *array, size_t size)
 	{
 		swapped = 0;
 
-		for (j = 0; j < size - i -1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				int temp = array[j];
+
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				swapped = 1;
@@ -31,7 +32,10 @@ void bubble_sort(int *array, size_t size)
 			}
 		}
 
-		 if (swapped == 0)
+		if (swapped == 0)
+		{
+
 			 break;
+		}
 	}
 }
