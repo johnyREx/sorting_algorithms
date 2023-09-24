fe3f02d45cc26195b91dd75d62b630a07e3caec9
johnyREx <johnkwofie99@icloud.com>
2023-09-24 00:48:32 -0700

    error fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,43 +3,53 @@
 /**
 * insertion_sort_list - Sorts a doubly linked list
 * of integers in ascending order
-* @list: A pointer to the head of the doubly linked list
+* @list: A lisyt to bhe sorted
 */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *prev, *tmp;
-	
+	listint_t *current, *temp, *p;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
-	
-	current = (*list)->next;
-	
-	while (current)
 	{
-		temp = current->next;
-		
-		while (current-> && current-> < current->prev->n)
+		return;
+	}
+
+	current = (*list)->next;
+
+	while (current != NULL)
+	{
+		temp = current->prev;
+		p = current;
+
+		while (temp != NULL && temp->n > p->)
 		{
-			prev = current->prev;
-			prev->next = current->next;
-			
-			if (current->next)
-				current->next->prev = prev;
-			current->next = prev;
-			current->prev = prev->prev;
-			prev->prev = current;
-			
-			if (current->prev)
-				current->prev->next = current;
-			
+			p->prev = temp->prev;
+
+			if (p->prev != NULL)
+			{
+				p->prev->next = p;
+			}
+
 			else
-				
-				*list = current;
-			
+			{
+				*list = p;
+			}
+
+			temp->next = p->next;
+
+			if (temp->next != NULL)
+			{
+				temp->next->prev = temp;
+			}
+
+			p->next = temp;
+			temp->prev = p;
+			temp = p->prev;
+
 			print_list(*list);
 		}
-		
-		current = temp;
+
+		current = current->next;
 	}
 }
